ec642ac4a1ee2bfb7842683f67bc62816e71cfca
Chengyu Sun <csun@calstatela.edu>
2020-10-30 15:01:03 -0700

    Updated Node.js examples

--- node_scripts/test3-callback.js
+++ node_scripts/test3-callback.js
@@ -1,23 +1,27 @@
-'use strict';
+"use strict";
 
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
-let users = [{
-    name: 'John',
-    email: 'john@localhost'
+let users = [
+  {
+    name: "John",
+    email: "john@localhost",
   },
   {
-    name: 'Jane',
-    email: 'jane@localhost'
-  }
+    name: "Jane",
+    email: "jane@localhost",
+  },
 ];
 
-MongoClient.connect('mongodb://localhost', (err, client) => {
-  let db = client.db('test3');
-  let collection = db.collection('users');
-  collection.remove({}, (err, result) => {
-    collection.insert(users, (err, result) => {
-      client.close();
+let dbUsername = "cs5220stu31";
+let dbPassword = "abcd";
+let dbUrl = `mongodb://${dbUsername}:${dbPassword}@localhost:4042/${dbUsername}`;
+
+MongoClient.connect(dbUrl, { useUnifiedTopology: true }, (err, client) => {
+  let db = client.db("cs5220stu31");
+  db.collection("users", {}, (err, collection) => {
+    collection.insertMany(users, (err, result) => {
+      client.close((err) => console.log("connection closed"));
     });
   });
 });
--- node_scripts/test3-promise.js
+++ node_scripts/test3-promise.js
@@ -1,24 +1,28 @@
-'use strict';
+"use strict";
 
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
-let users = [{
-    name: 'John',
-    email: 'john@localhost'
+let users = [
+  {
+    name: "John",
+    email: "john@localhost",
   },
   {
-    name: 'Jane',
-    email: 'jane@localhost'
-  }
+    name: "Jane",
+    email: "jane@localhost",
+  },
 ];
 
+let dbUsername = "cs5220stu31";
+let dbPassword = "abcd";
+let dbUrl = `mongodb://${dbUsername}:${dbPassword}@localhost:4042/${dbUsername}`;
+
 async function run() {
-  let client = await MongoClient.connect('mongodb://localhost');
-  let db = client.db('test3');
-  let collection = db.collection('users');
-  let result = await collection.remove({});
-  result = await collection.insert(users);
-  result = await client.close();
+  let client = await MongoClient.connect(dbUrl, { useUnifiedTopology: true });
+  let db = client.db("cs5220stu31");
+  let collection = await db.collection("users");
+  await collection.insertMany(users);
+  await client.close();
 }
 
 run();
